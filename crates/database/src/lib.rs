ceee56bcaca2aea4afee9f8b69766ee20a203efb
Borja Castellano <borjacastellano1@gmail.com>
2025-02-15 12:48:32 +0100

    socket client and server working

--- crates/database/src/lib.rs
+++ crates/database/src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use sqlx::sqlite::SqlitePoolOptions;
+use sqlx::SqlitePool;
+
+pub mod models;
+
+pub async fn create_conn_pool(db: &str) -> SqlitePool {
+    SqlitePoolOptions::new()
+        .max_connections(20)
+        .min_connections(5)
+        .connect(db)
+        .await
+        .expect("Failed to create pool")
 }
 
 #[cfg(test)]
-mod tests {
+mod test {
+    use std::env;
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[tokio::test]
+    async fn test_connection_pool() {
+        dotenvy::dotenv().ok();
+        let pool = create_conn_pool(&env::var("DATABASE_URL").unwrap()).await;
+
+        sqlx::query("CREATE TABLE users (id INTEGER PRIMARY KEY, name TEXT)")
+            .execute(&pool)
+            .await
+            .expect("Failed to create table");
+        
+        pool.close().await;
     }
 }
--- crates/server/src/main.rs
+++ crates/server/src/main.rs
@@ -1,3 +1,101 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::{LazyLock, OnceLock};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 2 {
+        eprintln!("Usage: {} <database-url> [port]", args[0]);
+        std::process::exit(1);
+    }
+
+    let db = &args[1];
+
+    let port = if args.len() == 3 {
+        args[2].parse().expect("Invalid port")
+    } else {
+        1237
+    };
+
+    start_server(db, port).await;
+}
+
+static DB_CONN: LazyLock<OnceLock<String>> = LazyLock::new(OnceLock::new);
+
+async fn start_server(db: &str, port: u16) {
+    let listener = tokio::net::TcpListener::bind(format!("127.0.0.1:{}", port))
+        .await
+        .expect("Could not bind");
+    
+    DB_CONN.set(db.to_string()).unwrap();
+    
+    loop {
+        match listener.accept().await {
+            Ok((socket, _)) => {
+                tokio::spawn(async move {
+                    handle_client(socket).await;
+                });
+            }
+            Err(e) => println!("couldn't get client: {:?}", e),
+        }
+    }
+}
+
+async fn handle_client(socket: tokio::net::TcpStream) {
+    let db = DB_CONN.get().unwrap();
+    let _ = database::create_conn_pool(db).await;
+
+    let (reader, writer) = tokio::io::split(socket);
+
+    let mut reader = tokio::io::BufReader::new(reader);
+    let mut writer = tokio::io::BufWriter::new(writer);
+
+    loop {
+        let mut buf = vec![0; 1024];
+        let size = reader.read(&mut buf).await.unwrap();
+        
+        let response = std::str::from_utf8(&buf[..size]).unwrap();
+        let response = response.trim();
+
+        writer.write_all(response.as_bytes()).await.unwrap();
+        writer.flush().await.unwrap();
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::thread::sleep;
+    use super::*;
+
+    #[tokio::test]
+    async fn test_handle_client() {
+        let db = "sqlite::memory:";
+
+        let _server = tokio::spawn(async move {
+            start_server(db, 1237).await;
+        });
+
+        sleep(std::time::Duration::from_secs(5));
+        
+        let mut client = tokio::net::TcpStream::connect("localhost:1237")
+            .await
+            .expect("Could not connect");
+
+        let (reader, writer) = tokio::io::split(client);
+
+        let mut reader = tokio::io::BufReader::new(reader);
+        let mut writer = tokio::io::BufWriter::new(writer);
+        
+        writer.write_all(b"Hello").await.unwrap();
+        writer.flush().await.unwrap();
+        
+        let mut buf = vec![0; 1024];
+        let size = reader.read(&mut buf).await.unwrap();
+        
+        let response = std::str::from_utf8(&buf[..size]).unwrap();
+        let response = response.trim();
+        
+        assert_eq!(response, "Hello");
+    }
 }
